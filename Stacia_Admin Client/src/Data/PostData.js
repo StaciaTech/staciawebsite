551b07be0d24386aabded061e931a6744bfcfcee
ShimiyonS <shimjnss@gmail.com>
2023-09-27 17:23:14 +0530

    Profile_Page_Update

---
+++ Stacia_Admin Client/src/Data/PostData.js
@@ -0,0 +1,46 @@
+export const post = [
+  {
+    content:
+      "We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
+    likeCount: "23",
+    shear: "10",
+    analyesd: "25",
+    original: false,
+    date: "jan-10/2023",
+  },
+  {
+    content:
+      "We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate future We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futureWe are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
+    likeCount: "23",
+    shear: "10",
+    analyesd: "25",
+    original: true,
+    date: "jan-10/2023",
+  },
+  {
+    content:
+      "We are confident that our partnership with Stacia We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futurewiWe are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futurell continue to yield positive results, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
+    likeCount: "23",
+    shear: "10",
+    analyesd: "25",
+    original: true,
+    date: "jan-10/2023",
+  },
+  {
+    content:
+      "We are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futuresults, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
+    likeCount: "23",
+    shear: "10",
+    analyesd: "25",
+    original: true,
+    date: "jan-10/2023",
+  },{
+    content:
+      "We are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnershiWe are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnership with Stacia will continue to yield positive rep with Stacia will continue to yield positive results, and we eagerly anticipate futuresults, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
+    likeCount: "23",
+    shear: "10",
+    analyesd: "25",
+    original: false,
+    date: "jan-10/2023",
+  },
+];
--- Stacia_Admin Client/src/pages/UserProfile.jsx
+++ Stacia_Admin Client/src/pages/UserProfile.jsx
@@ -7,7 +7,14 @@ import Typography from "@mui/material/Typography";
 import backGroungImage from "../assets/profileBackGround.svg";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import socialMediaIcons from "../assets/socialMediaIcons.svg";
+import { post } from "../Data/PostData";
+import like from "../assets/like.svg";
+import shear from "../assets/shear.svg";
+import status from "../assets/status.svg";
+import shearicon from "../assets/shearIcon.svg";
+
 export default function RecipeReviewCard() {
+  console.log(post);
   const theme = createTheme({
     typography: {
       fontFamily: "Inter",
@@ -27,7 +34,6 @@ export default function RecipeReviewCard() {
             image={DefaultImage}
             alt="Paella dish"
           />
-
           <CardContent>
             <p className="font-inter text-sm font-bold">ABOUT</p>
             <Typography variant="body2">
@@ -47,6 +53,55 @@ export default function RecipeReviewCard() {
             </div>
           </CardContent>
         </Card>
+      </div>
+      <div className=" max-w-screen-2xl min-w-min-50 pl-96 ml-20 ">
+        {post.map((items, id) => {
+          return (
+            <div className="" key={id}>
+              <div>
+                <p
+                  className="font-medium font-[Inter] p-3"
+                  style={{ fontSize: "14px" }}
+                >
+                  {items.content}
+                </p>
+              </div>
+              <div className=" grid grid-cols-8 pl-3">
+                <div className=" pr-14">
+                  <img src={like} alt="" className="inline pr-2 pb-1" />
+                  {items.likeCount}
+                </div>
+                <div className=" pr-14">
+                  <img src={shear} className="inline pr-2 pb-1" alt="" />
+                  {items.shear}
+                </div>
+                <div className=" pr-14">
+                  <img src={status} className="inline pr-2 pb-1" alt="" />
+                  {items.analyesd}
+                </div>
+                <div className=" pr-14">
+                  <img src={status} className="inline pr-2 pb-1" alt="" />
+                </div>
+                <div className=" pr-14">
+                  <img src={shearicon} className="inline pr-2 pb-1" alt="" />
+                </div>
+                <div>
+                  {items.original ? (
+                    <p className=" text-center  border-2 border-black rounded-full w-28 bg-black text-white">
+                      Original
+                    </p>
+                  ) : (
+                    <p className="text-center  border-2 border-black rounded-full w-28">
+                      Replied
+                    </p>
+                    
+                  )}
+                </div>
+              </div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     </ThemeProvider>
   );
