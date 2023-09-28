5af9ae703ac6ac3280b9a30a36d84cd7bc538d1e
ShimiyonS <shimjnss@gmail.com>
2023-09-28 17:32:49 +0530

    User_Profile_Update

--- Stacia_Admin Client/src/Data/PostData.js
+++ Stacia_Admin Client/src/Data/PostData.js
@@ -2,45 +2,55 @@ export const post = [
   {
     content:
       "We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
-    likeCount: "23",
+    likeCount: "68",
     shear: "10",
     analyesd: "25",
     original: false,
-    date: "jan-10/2023",
+    date: "jan-10/2014",
   },
   {
     content:
       "We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate future We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futureWe are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
-    likeCount: "23",
-    shear: "10",
+    likeCount: "74",
+    shear: "15",
     analyesd: "25",
     original: true,
-    date: "jan-10/2023",
+    date: "jan-10/2025",
   },
   {
     content:
       "We are confident that our partnership with Stacia We are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futurewiWe are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futurell continue to yield positive results, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
-    likeCount: "23",
-    shear: "10",
+    likeCount: "78",
+    shear: "58",
     analyesd: "25",
     original: true,
-    date: "jan-10/2023",
+    date: "jan-10/2032",
   },
   {
     content:
       "We are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnership with Stacia will continue to yield positive results, and we eagerly anticipate futuresults, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
-    likeCount: "23",
-    shear: "10",
+    likeCount: "88",
+    shear: "85",
     analyesd: "25",
     original: true,
-    date: "jan-10/2023",
-  },{
+    date: "jan-10/2020",
+  },
+  {
     content:
       "We are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnershiWe are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnership with Stacia will continue to yield positive rep with Stacia will continue to yield positive results, and we eagerly anticipate futuresults, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
-    likeCount: "23",
-    shear: "10",
+    likeCount: "18",
+    shear: "69",
     analyesd: "25",
     original: false,
-    date: "jan-10/2023",
+    date: "jan-10/2022",
+  },
+  {
+    content:
+      "We are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnershiWe are confident that our partnership with Stacia will continue to yield positive reWe are confident that our partnership with Stacia will continue to yield positive rep with Stacia will continue to yield positive results, and we eagerly anticipate futuresults, and we eagerly anticipate future collaborations.Please extend our appreciation to your entire team for their hard work and dedication.",
+    likeCount: "18",
+    shear: "69",
+    analyesd: "25",
+    original: false,
+    date: "jan-10/2022",
   },
 ];
