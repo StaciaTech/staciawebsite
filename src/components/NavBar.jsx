f1b8ad6f6746d05a4933bc657d6d9f3edc015dd3
varma1201 <vikramvarma1201@gmail.com>
2023-10-19 12:23:39 +0530

    removed-context

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,108 +1,38 @@
 import React from "react";
-import { useContext } from "react";
 import "../styles/navbar.css";
-import { useNavigate } from "react-router-dom";
 import StaciaLogo from "../assets/StaciaLogo.svg";
 import ContactIcon from "../assets/ContactIcon.svg";
-import { Context } from "../context/context";
+import { NavLink, Link } from "react-router-dom";
 
 function NavBar() {
-  const NavbarStates = useContext(Context);
-  console.log(NavbarStates.isServiceActive);
-
-  const {
-    isServiceActive,
-    setIsServiceActive,
-    isProductActive,
-    setIsProductActive,
-    isProjectActive,
-    setIsProjectActive,
-    isCareerActive,
-    setIsCareerActive,
-  } = NavbarStates;
-
-  const navigate = useNavigate();
-
-  const homeHandler = () => {
-    setIsServiceActive(false);
-    setIsProductActive(false);
-    setIsProjectActive(false);
-    setIsCareerActive(false);
-    navigate("/");
-  };
-  const serviceActiveHandler = () => {
-    setIsServiceActive(true);
-    setIsProductActive(false);
-    setIsProjectActive(false);
-    setIsCareerActive(false);
-    navigate("/service");
-  };
-  const productActiveHandler = () => {
-    setIsServiceActive(false);
-    setIsProductActive(true);
-    setIsProjectActive(false);
-    setIsCareerActive(false);
-    navigate("/product");
-  };
-  const projectactiveHandler = () => {
-    setIsServiceActive(false);
-    setIsProductActive(false);
-    setIsProjectActive(true);
-    setIsCareerActive(false);
-    navigate("/project");
-  };
-  const careerActiveHandler = () => {
-    setIsServiceActive(false);
-    setIsProductActive(false);
-    setIsProjectActive(false);
-    setIsCareerActive(true);
-    navigate("/career");
-  };
-
   return (
     <div className="nav-container">
       <div className="nav-items-container">
         <div className="nav-left">
-          <img
-            src={StaciaLogo}
-            alt="Home"
-            className="nav-items"
-            onClick={homeHandler}
-          />
-          <div
-            className="nav-items"
-            onClick={serviceActiveHandler}
-            style={isServiceActive ? { opacity: 1 } : { opacity: 0.5 }}
-          >
+          <Link to={"/"}>
+            <img src={StaciaLogo} alt="Home" />
+          </Link>
+
+          <NavLink to={"/service"} className="nav-items">
             Services
-          </div>
-          <div
-            className="nav-items"
-            onClick={productActiveHandler}
-            style={isProductActive ? { opacity: 1 } : { opacity: 0.5 }}
-          >
+          </NavLink>
+          <NavLink to={"/product"} className="nav-items">
             Products
-          </div>
-          <div
-            className="nav-items"
-            onClick={projectactiveHandler}
-            style={isProjectActive ? { opacity: 1 } : { opacity: 0.5 }}
-          >
+          </NavLink>
+          <NavLink to={"/project"} className="nav-items">
             Projects
-          </div>
+          </NavLink>
           <div className="nav-items" style={{ opacity: 0.5 }}>
             Resources
           </div>
-          <div
-            className="nav-items"
-            onClick={careerActiveHandler}
-            style={isCareerActive ? { opacity: 1 } : { opacity: 0.5 }}
-          >
+          <NavLink to={"/career"} className="nav-items">
             Careers
-          </div>
+          </NavLink>
         </div>
         <div className="nav-right">
-          <div className="nav-item-whatsnew">What'sNew</div>
+          <Link to={"/whatsNew"} className="nav-item-whatsnew">
+            What'sNew
+          </Link>
           <div
             style={{
               height: "48px",
--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Provides } from "./context/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provides>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provides>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
